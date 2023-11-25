4aee8afab74d78ebbbe2bdee57cdadb901ec80b6
ChanMor <morelos.franzchristian@gmail.com>
2023-11-26 01:47:58 +0800

    aligned using two frames

--- solvers/polynomialRegression.py
+++ solvers/polynomialRegression.py
@@ -11,49 +11,55 @@ class PolynomialRegressionPage(ttk.Frame):
         self.file = StringVar()
 
         polynomial_regression_label = ttk.Label(self, text="Polynomial Regression Solver", font=("Arial", 28), bootstyle="default")
-        polynomial_regression_label.grid(row=0, column=0, columnspan=4, pady=10)
+        polynomial_regression_label.pack(pady=20, anchor="w")
 
         page_title_separator = ttk.Separator(self, orient='horizontal', style='secondary.Horizontal.TSeparator')
-        page_title_separator.grid(row=1, column=0, columnspan=4, sticky="ew", pady=10)
+        page_title_separator.pack(fill="both", expand=True)
 
-        back_btn = ttk.Button(self, text="Back", bootstyle="light-outline", command=lambda: send_to(generic_solver))
-        back_btn.grid(row=2, column=1, pady=10, sticky="w")
+        frame_right = ttk.Frame(self)
+        frame_right.pack(side="right", padx=10)
 
-        self.result_canvas = ttk.Canvas(self, width=400, height=100, bg='white')
-        self.result_canvas.grid(row=3, rowspan=4, column=2, columnspan=2, sticky="w")
+        self.result_canvas = ttk.Canvas(frame_right, width=400, height=300, bg='white')
+        self.result_canvas.pack(side="top", pady=5)
 
-        degree_label = ttk.Label(self, text="Degree:")
-        degree_label.grid(row=3, column=1, pady=10, sticky="w")
+        frame_left = ttk.Frame(self)
+        frame_left.pack(side="left", padx=10)
 
-        self.degree_entry = ttk.Entry(self, width=40)
-        self.degree_entry.grid(row=4, column=1, columnspan=3, pady=10, padx=10, sticky="w")
+        back_btn = ttk.Button(frame_left, text="Back", bootstyle="light-outline", command=lambda: send_to(generic_solver))
+        back_btn.pack(pady=20, anchor="w")
 
-        data_label_x = ttk.Label(self, text="Data Points X (comma-separated or CSV file):")
-        data_label_x.grid(row=5, column=1, pady=10, sticky="w")
+        degree_label = ttk.Label(frame_left, text="Degree:")
+        degree_label.pack(pady=5, anchor="w")
 
-        self.data_entry_x = ttk.Entry(self, textvariable=self.file_x, width=40)
-        self.data_entry_x.grid(row=6, column=1, columnspan=3, pady=10, padx=10, sticky="w")
+        self.degree_entry = ttk.Entry(frame_left, width=40)
+        self.degree_entry.pack(pady=5)
 
-        data_label_y = ttk.Label(self, text="Data Points Y (comma-separated or CSV file):")
-        data_label_y.grid(row=7, column=1, pady=10, sticky="w")
+        data_label_x = ttk.Label(frame_left, text="Data Points X (comma-separated or CSV file):")
+        data_label_x.pack(pady=5, anchor="w")
 
-        self.data_entry_y = ttk.Entry(self, textvariable=self.file_y, width=40)
-        self.data_entry_y.grid(row=8, column=1, columnspan=3, pady=10, padx=10, sticky="w")
+        self.data_entry_x = ttk.Entry(frame_left, textvariable=self.file_x, width=40)
+        self.data_entry_x.pack(pady=5)
 
-        estimate_label = ttk.Label(self, text="Estimate: ")
-        estimate_label.grid(row=9, column=1, pady=10, sticky="w")
+        data_label_y = ttk.Label(frame_left, text="Data Points Y (comma-separated or CSV file):")
+        data_label_y.pack(pady=5, anchor="w")
 
-        self.estimate_entry = ttk.Entry(self, width=40)
-        self.estimate_entry.grid(row=10, column=1, columnspan=3, pady=10, padx=10, sticky="w")
+        self.data_entry_y = ttk.Entry(frame_left, textvariable=self.file_y, width=40)
+        self.data_entry_y.pack(pady=5)
 
-        upload_csv_btn = ttk.Button(self, text="Upload CSV file", bootstyle="light-outline", command=self.browse_csv)
-        upload_csv_btn.grid(row=11, column=1, pady=10, padx=10, sticky="w")
+        estimate_label = ttk.Label(frame_left, text="Estimate: ")
+        estimate_label.pack(pady=5, anchor="w")
 
-        calculate_btn = ttk.Button(self, text="Calculate", bootstyle="primary", command=self.calculate_polynomial)
-        calculate_btn.grid(row=11, column=1, pady=10, padx=160, sticky="w")
+        self.estimate_entry = ttk.Entry(frame_left, width=40)
+        self.estimate_entry.pack(pady=5)
 
-        reset_btn = ttk.Button(self, text="Reset", bootstyle="danger", command=self.reset)
-        reset_btn.grid(row=11, column=1, pady=10, padx=275, sticky="w")
+        upload_csv_btn = ttk.Button(frame_left, text="Upload CSV file", bootstyle="light-outline", command=self.browse_csv)
+        upload_csv_btn.pack(side="left", pady=20)
+
+        calculate_btn = ttk.Button(frame_left, text="Calculate", bootstyle="primary", command=self.calculate_polynomial)
+        calculate_btn.pack(side="left", pady=5, padx=30)
+
+        reset_btn = ttk.Button(frame_left, text="Reset", bootstyle="danger", command=self.reset)
+        reset_btn.pack(side="left", pady=5)
 
     def browse_csv(self):
         self.data_entry_x.config(state="disabled")
@@ -105,8 +111,8 @@ class PolynomialRegressionPage(ttk.Frame):
             estimate_result = estimate_polynomial_regression(degree, data, estimate_data)
             
             self.result_canvas.delete("all")
-            self.result_canvas.create_text(10, 10, anchor="nw", text=f"Polynomial Function: {result}\nEstimate at {estimate_data}: {estimate_result}", width=380, fill="lightgrey")
+            self.result_canvas.create_text(10, 10, anchor="nw", text=f"Polynomial Function:\n {result}\n\nEstimate at {estimate_data}:\n {estimate_result}", width=400, fill="lightgrey")
 
         except ValueError:
             self.result_canvas.delete("all")
-            self.result_canvas.create_text(10, 10, anchor="nw", text="Invalid input. Please check your input values.", width=380, fill="lightcoral")
+            self.result_canvas.create_text(10, 10, anchor="nw", text="Invalid input. Please check your input values.", width=400, fill="lightcoral")
